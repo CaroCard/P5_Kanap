496ecd57a1cc0d59b4328e87789e03dd89b6d267
CaroCard <leroca83@hotmail.fr>
2021-11-02 15:10:20 +0100

    Evolution page panier + blocage

--- front/js/product.js
+++ front/js/product.js
@@ -30,12 +30,58 @@ fetch (fetch_Url_Id)
     // Créer une fonction qui parcourt les couleurs et les affiches une à une chaque fois en ajoutant un élément option
     for (let i of value.colors){
         const newOption = document.createElement("option");
-        // LES COULEURS NE S'AFFICHENT PAS EN TEXTE
-        newOption.textContent = value.colors.i;
+
+        newOption.textContent = i;
         newOption.setAttribute('value', value.colors.i);
+
         document.getElementById('colors').appendChild(newOption);
     }
 })
 .catch(function (err){
     console.log("bwahaha tu t'es planté!")
 });
+
+//
+// CODE POURRI SI DESSOUS =)
+//
+
+// addEventListener sur couleur et récup donnée ${event.target.value}
+const colorSelect = document.querySelector('#colors');
+
+colorSelect.addEventListener('change', function(e){
+
+    const colorChoice = e.target.value;
+    console.log(colorChoice); // undefined
+})
+
+// addEventListener sur quantité et récup donnée ${event.target.value}
+const quantityInput = document.querySelector('#quantity');
+
+quantityInput.addEventListener('change', function(e){
+
+    const quantityChoice = e.target.value;
+    console.log(quantityChoice); // undefined
+})
+
+//
+// CODE POURRI SI DESSUS =)
+//
+
+const buttonAddToCart = document.querySelector('#addToCart');
+
+buttonAddToCart.addEventListener('click', function(e){
+    e.preventDefault();
+
+    localStorage.id = id;
+    const recupId = localStorage.id; // check
+    console.log(recupId);   // check
+
+
+
+});
+    // Add au localStorage: id, couleur, quantité
+    // Ajout    localStorage.prenom = "dany";
+    // Récup    localStorage.prenom;
+
+    /* Si stockage en objet, "kanap-"i (i = localStorage.length +1) poru gérer l'ajout de plusieurs canapés...logique pas évidente
+    */
